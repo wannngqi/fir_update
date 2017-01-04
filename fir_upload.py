ee40b466ef5e04b8c5368406500ab210e22480a1
wangqi <wannngqi@163.com>
2017-01-04 23:19:03 +0800

    complete curl cmd

--- fir_upload.py
+++ fir_upload.py
@@ -9,8 +9,8 @@ changelog = input('log:')
 fir_url = "http://api.fir.im/apps"
 headers = {"Content-Type": "application/json"}
 body = {"type":"ios",
-        "bundle_id":"",
-        "api_token":"",
+        "bundle_id":"test.com",
+        "api_token":"44018a977fe5d14384e5f598497f5d72",
         }
 rsp = requests.post(fir_url,headers = headers,json=body)
 
@@ -20,9 +20,9 @@ rsp_json = json.loads(rsp.text)
 
 # print('binary = '+rsp_json['cert']['binary']['token'])
 
-update_headers = {'Content-Type': 'multipart/form-data; boundary=----WebKi',
-                  # 'Content - Disposition': 'form - data;name = \'MGISRelease.ipa\''
-                  }
+# update_headers = {'Content-Type': 'multipart/form-data; boundary=----WebKi',
+#                   # 'Content - Disposition': 'form - data;name = \'MGISRelease.ipa\''
+#                   }
 # file_handle = open(file_path,'rb')
 # try:
 #     data = file_handle.read()
@@ -38,19 +38,30 @@ update_headers = {'Content-Type': 'multipart/form-data; boundary=----WebKi',
 #                'x:changelog':changelog}
 
 update_url = rsp_json['cert']['binary']['upload_url']
-# print('update_url:'+update_url)
+print('update_url:'+update_url)
 # update_rsp = requests.post(update_url,json=binary_body,headers=update_headers)
 # print(update_rsp.text)
 
-cmd_curl = 'curl   -F    \"key='+ rsp_json['cert']['binary']['key']+'\"'
-       # -F "token=xxxxx"             \
-       # -F "file=@aa.apk"            \
-       # -F "x:name=aaaa"             \
-       # -F "x:version=a.b.c"         \
-       # -F "x:build=1"               \
-       # -F "x:release_type= Adhoc"   \  #type=ios 使用
-       # -F "x:changelog=first"       \
-       # http://upload.qiniu.com'
+cmd_curl = 'curl   -F    \"key='+ rsp_json['cert']['binary']['key']+'\"' +'\\' +'\n' +\
+       ' -F \"token='+rsp_json['cert']['binary']['token']+'\"'+'\\'+'\n' +\
+       ' -F \"file='+file_path+'\"'+'\\'+'\n' +\
+       ' -F \"x:name=aaaa\"' +'\\'+'\n' +\
+       ' -F \"x:version=1.0.0\"'+'\\' +'\n'        +\
+       ' -F \"x:build=1\"'  +'\\'+  '\n'           +\
+       ' -F \"x:release_type= Adhoc\"' +'\\'+  '\n' +\
+       ' -F \"type=ios\"'  '\n'  +\
+       ' -F \"x:changelog=first\"' +'\\'+ '\n'       +\
+       'upload.qiniu.com'# update_url
 
+# cmd_curl = 'curl   -F    \"key='+ rsp_json['cert']['binary']['key']+'\"\n' +' -F \"token='+rsp_json['cert']['binary']['token']+'\"\n' +' -F \"file='+file_path            +'\"\n' +\
+#        ' -F \"x:name=aaaa\"' +'\n' +\
+#        ' -F \"x:version=1.0.0\"' +'\n'        +\
+#        ' -F \"x:build=1\"'    '\n'           +\
+#        ' -F \"x:release_type= Adhoc\"'   '\n' +\
+#        ' -F \"type=ios \"'  '\n'  +\
+#        ' -F \"x:changelog=first\"'  '\n'       +\
+#        update_url
+
+print('cmd:'+cmd_curl)
 os.system(cmd_curl)
 
